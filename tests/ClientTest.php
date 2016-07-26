d7deaa704ce8ec3bc64738b6d754fa2e8e760b6c
Gamaliel Espinoza <gamaliel.espinoza@gmail.com>
2016-07-26 16:10:21 -0700

    Nuevo token de eSMS.

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -6,13 +6,18 @@ use esms\http\Client;
 
 class ClientTest extends \PHPUnit_Framework_TestCase {
 
-    const TOKEN = "KTzGLtJa_94rzI2dahQ96qIGaiRBODEV-rNPIbHNgi6tQUY_jC-05PP6SpqDLN8W";
-    const SECRET = "Rv-iE9mBt31CQimHs1K53W9kYohSaSbICmOa7T7Qj8B2Hmq3lPLmz8SVhEdinwA6";
+    const TOKEN = "c-vfp3hudNNrF0ecPYTp8QgCYElSm9qU0QlGJHklWruV4TBcB1FmVnRMiUpZN4CF";
+    const SECRET = "XnQs-fZUu_X-aVAFR8UVYWNW0VQNcFeh_YKXD0ZMsrWXdax-S-1Ur8pN0prVBkPr";
     const TARGET = "6621207135";
 
     public function testSendMessage() {
         $client = new Client(self::TOKEN, self::SECRET);
-        $client->enviarMensaje(self::TARGET, 'Hola Mundo!');
+        $response = $client->enviarMensaje(self::TARGET, 'El fin del mundose acerca!');
+        $this->objectHasAttribute('success', $response);
+        $this->assertTrue($response->success);
+        $this->objectHasAttribute('mensaje', $response);
+        $this->objectHasAttribute('id', $response->mensaje);
+        $this->assertGreaterThan(0, $response->mensaje->id);
     }
 
     public function testObtain() {
